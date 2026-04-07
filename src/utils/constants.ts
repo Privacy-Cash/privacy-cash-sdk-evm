4339207aa9f878095f9598406a133037b496a02b
adrianmcole <264029017+adrianmcole@users.noreply.github.com>
2026-04-07 17:18:26 +0800

    new contract

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = '0xBcFfcE41652093264D0807e2E834afF255982c98';
+export const CONTRACT_ADDRESS = '0x7F673790C08Ddf27c0Aa6fa9526CCC8dAaB081Ec';
 export const FEE_RECIPIENT_ADDRESS = '0x44eb9939cfdE7C394f1632C6890191d695f0a3ce';
 export const INDEXER_URL = process.env.NEXT_PUBLIC_BASE_INDEXER_URL || 'https://evm.privacycash.org';
 export const BASE_SEPOLIA_RPC = process.env.NEXT_PUBLIC_BASE_RPC || 'https://mainnet.base.org';
